ea83187081e126ac58b6c769eb5b69d460643f02
Joel <bacnl@vnu.edu.vn>
2017-06-12 17:24:45 +0700

    init

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var youtube = require('youtube-finder')
+var client = youtube.createClient({ key: 'AIzaSyA7Tw0_fNOkYTyJX02wOewbstjGDAJ86U0'})
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var params = {
+    part: 'snippet',
+    q: '',
+    maxResults: 5,
+    type : 'video'
+  }
+  var urls = [];
+  client.search(params, function (err, data) {
+    data.items.forEach(function(element) {      
+        urls.push(element.id.videoId);
+    }, this);    
+    res.render('index', { urls:urls });
+  })
+  
+});
+router.post('/', function(req, res, next) {
+  var params = {
+    part: 'snippet',
+    q: req.body.keyword,
+    maxResults: 5,
+    type : 'video'
+  }
+  var urls = [];
+  client.search(params, function (err, data) {
+    data.items.forEach(function(element) {     
+        urls.push(element.id.videoId);
+    }, this);    
+    res.render('index', { urls:urls });
+  })
+  
+});
+
+module.exports = router;
